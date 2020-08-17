b7d648fc4251d9ef2b1dda00769063b7a8c7d5f4
Princerajsdv81 <60143366+Princerajsdv81@users.noreply.github.com>
2020-08-17 16:32:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 sudo cd /root/jenkins_groovy
 sudo ls
-export len1=\$(ls -l /var/lib/jenkins/workspace/deploy_job2 | grep html | wc -l)
+export len1=\$(ls -l /var/lib/jenkins/workspace/deploy_job2 | grep html )
 
 if [ \$len1 -gt 0 ]
 then
-	export len2=\$(sudo kubectl get deployments | grep webserver | wc -l)
+	export len2=\$(sudo kubectl get deployments | grep webserver )
 	if [ \$len2 -gt 0 ]
 	then
 		sudo kubectl rollout restart deployment/webserver
