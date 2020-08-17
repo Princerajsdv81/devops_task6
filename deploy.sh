c21a5a147dbfe2515e05b480bf5b34b465a58ff2
Princerajsdv81 <60143366+Princerajsdv81@users.noreply.github.com>
2020-08-17 17:38:32 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-sudo cd /home/jenkins_groovy
+sudo cd /root/jenkins_groovy
 sudo ls
 if sudo kubectl get all|grep webdeploy
 then
 sudo kubectl delete all --all
 sudo kubectl delete pvc --all
-sudo kubectl create -f /home/jenkins_groovy/webdeploy.yml
+sudo kubectl create -f /root/jenkins_groovy/webdeploy.yml
 sleep 10
 sudo kubectl get all
 else
-sudo kubectl create -f /home/jenkins_groovy/webdeploy.yml
+sudo kubectl create -f /root/jenkins_groovy/webdeploy.yml
 sleep 10
 sudo kubectl get all
 fi
-sudo kubectl cp /home/jenkins_groovy/index.html $(sudo kubectl get pod|grep webdeploy| awk '{print $1}'):/var/www/html
+sudo kubectl cp /root/jenkins_groovy/index.html $(sudo kubectl get pod|grep webdeploy| awk '{print $1}'):/var/www/html
 
